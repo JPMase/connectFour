900885a10d2cb095f72b2bcb5571290d103ca9f1
Jordan <mason.jordanp@gmail.com>
2017-07-25 15:57:00 -0500

    Added npm package for Mocha Chai and wrote placeholders for tests

--- test/scriptTest.js
+++ test/scriptTest.js
@@ -1,3 +1,41 @@
-// var chai = require('chai');
+var assert = chai.assert;
 
+describe('Buttons', function() {
+  it('Should change board colors');
 
+  it('Should reset play count');
+
+  it('Should reset red and black plays');
+
+  it('Should reset potential plays')
+});
+
+describe('Algorithms', function() {
+  it('Should sort passed in arrays');
+
+  it('Should find horizontal connect 4s');
+
+  it('Should find vertical connect 4s');
+
+  it('Should find diagonal left connect 4s');
+
+  it('Should find diagonal right connect 4s');
+});
+
+describe('Moves', function() {
+  it('Should have 42 possible moves');
+
+  it('Should change colors on moves');
+
+  it('Should increase play counter on moves');
+
+  it('Should store red moves on red plays');
+
+  it('Should store black moves on black plays');
+
+  it('Should check for winners after 7 moves');
+
+  it('Should check for only red winners on even moves');
+
+  it('Should check for only black winners on odd moves');
+});
